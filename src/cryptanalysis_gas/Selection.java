7f5d4633a34a17c150fb40d0a3cbcebd2d1860e4
AbdulllahTaher93 <abdullahtaher@correo.ugr.es>
2020-07-20 13:21:28 +0200

    closed #16

--- src/cryptanalysis_gas/Selection.java
+++ src/cryptanalysis_gas/Selection.java
@@ -18,13 +18,46 @@ public class Selection {
   String selecplain[]=new String[6];
   String selecplain2[]=new String[6];
   public Selection(double[] Fitness,String[] PlainText,int[][] Key) {
-     indexofkey=new int[Fitness.length];
-   keywithsort=new int[Key.length][Key[0].length];  
-   seleckey=new int[Key.length/2][Key[0].length];
-   seleckey2=new int[Key.length/2][Key[0].length];
-   selecplain=new String[PlainText.length/2];
-   selecplain2=new String[PlainText.length/2];
+    indexofkey=new int[Key.length];
+    keywithsort=new int[Key.length][Key[0].length];  
+    seleckey=new int[Key.length/2][Key[0].length];
+    seleckey2=new int[Key.length/2][Key[0].length];
+    selecplain=new String[PlainText.length/2];
+    selecplain2=new String[PlainText.length/2];
+    for(int i=Key.length-1;i>=0;i--)
+        indexofkey[i]=i; 
+    
+    Shell_Sort(Fitness,PlainText);
+    
   }
+
+    private void Shell_Sort(double[] Fitness, String[] PlainText) {
+        int h=Fitness.length/2,z;
+        double x;
+        String y;
+        for(int i=h;i>=0;i--)
+    {
+        for(int j=0;i+j<Fitness.length;j++)
+        if(Fitness[j]>Fitness[j+i])
+        {
+            x=Fitness[j];
+            Fitness[j]=Fitness[j+i];
+            Fitness[i+j]=x;
+            
+            y=PlainText[j];
+            PlainText[j]=PlainText[j+i];
+            PlainText[i+j]=y;
+            
+            
+            z=indexofkey[j];
+            indexofkey[j]=indexofkey[j+i];
+            indexofkey[i+j]=z;
+            
+            
+        }
+      
+   }
+    }
     
     
     
