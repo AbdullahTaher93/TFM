58b594342b89d8a496b1fcbf4391e40b319f6e75
AbdulllahTaher93 <abdullahtaher@correo.ugr.es>
2020-07-22 13:46:38 +0200

    create mutation method to generate two number randomly from keys and change their positions by these numbers, closed #27

--- src/cryptanalysis_gas/Mutation.java
+++ src/cryptanalysis_gas/Mutation.java
@@ -21,10 +21,31 @@ public class Mutation {
     public Mutation(String Plain_Text[],int keys[][]) {
          Array_Of_Plain=new String[Plain_Text.length];
          key=new int[keys.length][keys[0].length];
+         cross=new CrossOver();
+         Random rand=new Random();  
+          do{
+          
+                x=1+rand.nextInt(keys[0].length);
+                y=1+rand.nextInt(keys[0].length);
+            }while (x==y);
+          
+           int n1,n2;
+        System.out.println("The First Number="+x+"\nThe Second Number="+y);
+        for(int i=0;i<keys.length;i++)
+        {
+           n1= keys[i][x-1];
+           n2= keys[i][y-1];
+           keys[i][x-1]=n2;
+           keys[i][y-1]=n1;
+           
+        }
+        
+         
          
         
+        
     }
-   
+    
  }   
     
     
